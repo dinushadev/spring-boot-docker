3a9dec931cffdeb206fd4f33044a80f0241464dc
Dinusha <dnsw83@gmail.com>
2016-01-14 14:11:29 +0530

    remove commented codes

--- src/main/java/com/dns/docker/controller/UserController.java
+++ src/main/java/com/dns/docker/controller/UserController.java
@@ -17,15 +17,7 @@ public class UserController {
 	@Autowired
 	IUserDAO userRepo;
 	
-	/*@Autowired
-	IDocDAO docRepo;
-	
-	@Autowired
-	IPatientDAO patientRepo;
-	
-	@Autowired
-	IAdminDAO adminRepo;
-*/
+
 	@RequestMapping("/users")
 	public Iterable<User> users() {
 		Iterable<User> userList = userRepo.findAll();
@@ -36,38 +28,5 @@ public class UserController {
 		return userList;
 
 	}
-/*	
-	@RequestMapping("/doctors")
-	public Iterable<Doctor> doctors() {
-		Iterable<Doctor> docList = docRepo.findAll();
-		
-		for (User user : docList) {
-			System.out.println("userId:"+user.getUid()+" email:"+user.getEmail());
-		}
-		return docList;
 
-	}
-	
-	@RequestMapping("/patients")
-	public Iterable<Patient> patients() {
-		Iterable<Patient> patientList = patientRepo.findAll();
-		
-		for (User user : patientList) {
-			System.out.println("userId:"+user.getUid()+" email:"+user.getEmail());
-		}
-		return patientList;
-
-	}
-	
-	@RequestMapping("/admins")
-	public Iterable<Admin> admins() {
-		Iterable<Admin> patientList = adminRepo.findAll();
-		
-		for (User user : patientList) {
-			System.out.println("userId:"+user.getUid()+" email:"+user.getEmail());
-		}
-		return patientList;
-
-	}
-*/
 }
